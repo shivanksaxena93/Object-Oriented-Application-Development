6400244f7e34a65df6dc1098c21ac9793c92bc9f
ssaxena12 <35405266+ssaxena12@users.noreply.github.com>
2019-01-03 13:35:20 -0800

    Add files via upload

---
+++ BankLoan_Lab_02/src/BankRecordsTest.java
@@ -0,0 +1,13 @@
+
+public class BankRecordsTest {
+
+	public static void main(String[] args) {
+		BankRecords g = new BankRecords();
+		g.readData();
+		g.processData();
+		g.printData();
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ BankLoan_Lab_02/src/Client.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @author Shivank
+ *@version 1.3
+ *@since 2017/10/01
+ */
+
+public abstract class Client
+{
+	public  abstract void readData();
+	public  abstract void processData();
+	public abstract void printData();
+	
+
+}
